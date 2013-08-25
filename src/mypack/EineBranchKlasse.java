90152336468c8be646f95cb702ad9fe146b70c61
kumat <matthias.kuehnemann@freenet.de>
2013-08-25 09:28:53 +0200

    Klasse EineBranchKlasse hinzugefügt

---
+++ src/mypack/EineBranchKlasse.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class EineBranchKlasse {
+
+	
+	public EineBranchKlasse() {
+		// TODO Auto-generated constructor stub
+		System.out.println("ich existiere nur in einNeuerBranch");
+	}
+}
